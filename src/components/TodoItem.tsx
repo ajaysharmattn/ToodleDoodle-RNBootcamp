eefd4c188d671f990dae61aeb86456a021669b2c
Ajay Sharma <ajay.sharma3@tothenew.com>
2025-07-03 15:14:34 +0530

    added todo-item tile

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function TodoItem({item}: {item: string}) {
+    console.log('TodoItem rendered with item:', item);
+    return (
+        <View style={styles.todoItem}>
+            <Text style={styles.todoText}>{item}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    todoItem: {
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc'
+    },
+    todoText: {
+        fontSize: 16,
+        color: '#1b1616'
+    }
+})
