bce11a7adc2aeb8955e428ea9fa41aba93f5b8b3
Ajay Sharma <ajay.sharma3@tothenew.com>
2025-07-03 15:59:59 +0530

    refactored todo list to use Flatlist

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,21 +1,44 @@
-import { StyleSheet, View } from "react-native"
-import TodoItem from "./TodoItem"
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import TodoItem from './TodoItem';
 
-export default function TodoList({list}: {list: string[]}) {
-    return(
-        <View style={styles.container}>
-            {list.map((todo, index) => (
-                <TodoItem key={index} item={todo} />
-            ))}
-        </View>
-    )
+export default function TodoList({ list }: { list: string[] }) {
+  return (
+    <FlatList
+      style={styles.container}
+      data={list}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({ item }) => <TodoItem item={item} />}
+      contentContainerStyle={{ paddingBottom: 20 }}
+      ListEmptyComponent={EmptyListComponent}
+    />
+  );
+}
+
+function EmptyListComponent() {
+  return (
+    <View style={styles.emptyListContainer}>
+      <Text style={styles.emptyListText}>
+        No Todos available. Please add some!
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#e6dcdc',
-        borderRadius: 40,
-        padding: 20,
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#e6dcdc',
+    borderRadius: 40,
+    padding: 20,
+  },
+  emptyListContainer: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  emptyListText: {
+    color: '#1b1616',
+    fontSize: 16
+  },
+});
--- src/screens/Todos.tsx
+++ src/screens/Todos.tsx
@@ -1,17 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import TodoList from "../components/TodoList";
 import { useTodo } from "../utility/todo-context";
 
 export default function TodosScreen() {
     const { todos } = useTodo();
-
-    if (!todos || todos.length === 0) {
-        return (
-            <View>
-                <Text>No Todos available. Please add some!</Text>
-            </View>
-        );
-    }
 
     return (
         <View style={styles.container}>
