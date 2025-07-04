60c7ca3dd42998dc8c403c8dd45987f359e8c3c4
Ajay Sharma <ajay.sharma3@tothenew.com>
2025-07-04 12:01:51 +0530

    added contants for strings, navigation paths, colors, etc

---
+++ src/utility/Constants.ts
@@ -0,0 +1,20 @@
+export enum NavigatorScreenNames {
+    HOME = 'Home',
+    ADD_TODO = 'AddTodo',
+    TODOS = 'Todos',
+}
+
+export enum AppStrings {
+    HOME_SCREEN_TITLE = 'Welcome to ToodleDoodle 😊',
+    ADD_TODO_SCREEN_TITLE = 'Add a new Todo 😉',
+    ADD_TODO_ALERT_SUCCESS_TITLE = 'Todo Added',
+    ADD_TODO_ALERT_SUCCESS_MESSAGE = 'Your todo has been added successfully!',
+    ADD_TODO_ALERT_OK_TEXT = 'OK',
+    ADD_TODO_BUTTON_TEXT = 'Add Todo',
+    ADD_TODO_INPUT_PLACEHOLDER = 'Enter your todo here',
+    EMPTY_LIST_MESSAGE = 'No todos available. Please add a todo.',
+}
+
+export enum Colors {
+    PRIMARY = '#e6dcdc'
+}
