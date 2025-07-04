7acc0d4aef0bcfd11baadf2893879d37ef6e02a8
Ajay Sharma <ajay.sharma3@tothenew.com>
2025-07-04 12:01:07 +0530

    replaced native-stack with stack navigator & replaced constants with enums from constants.ts

--- src/utility/Navigator.tsx
+++ src/utility/Navigator.tsx
@@ -1,23 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/Home';
 import AddTodoScreen from '../screens/AddTodo';
 import TodosScreen from '../screens/Todos';
+import { NavigatorScreenNames } from './Constants';
 
 export type RootStackParamList = {
-  Home: undefined;
-  AddTodo: undefined;
-  Todos: undefined;
+  [NavigatorScreenNames.HOME]: undefined;
+  [NavigatorScreenNames.ADD_TODO]: undefined;
+  [NavigatorScreenNames.TODOS]: undefined;
 };
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createStackNavigator<RootStackParamList>();
 
 function RootStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="AddTodo" component={AddTodoScreen} />
-      <Stack.Screen name="Todos" component={TodosScreen} />
+      <Stack.Screen name={NavigatorScreenNames.HOME} component={HomeScreen} />
+      <Stack.Screen name={NavigatorScreenNames.ADD_TODO} component={AddTodoScreen} />
+      <Stack.Screen name={NavigatorScreenNames.TODOS} component={TodosScreen} />
     </Stack.Navigator>
   );
 }
